f04b9a65ad7e89692841f6c994042b36eff05f9c
amitjoshi9627 <amitjoshi9627@gmail.com>
2020-01-24 12:18:57 +0530

    Minor changes

--- utils.py
+++ utils.py
@@ -4,36 +4,36 @@ import getpass
 
 
 def parse_sending_options(opt, msg1, msg2):
-    sender_email, sender, receiver_email, receiver = take_credentials()
+    sender_email, sender_pwd, sender, receiver_email, receiver = take_credentials()
     if opt == 1:
         message_type = "News Headlines"
         send_mail(msg1, message_type, sender_email,
-                  sender, receiver_email, receiver)
+                  sender, receiver_email, receiver, sender_pwd)
     elif opt == 2:
         message_type = "Weather Forecast"
         send_mail(msg2, message_type, sender_email,
-                  sender, receiver_email, receiver)
+                  sender, receiver_email, receiver, sender_pwd)
     else:
         message_type1 = "News Headlines"
         message_type2 = "Weather Forecast"
         send_mail(msg1, message_type1, sender_email,
-                  sender, receiver_email, receiver)
+                  sender, receiver_email, receiver, sender_pwd)
         send_mail(msg2, message_type2, sender_email,
-                  sender, receiver_email, receiver)
+                  sender, receiver_email, receiver, sender_pwd)
     print("Success!!")
 
 
 def take_credentials():
-    sender_email = input("Enter Sender's email id:")
+    sender_email = input("Enter Sender's email id: ")
     sender_pwd = getpass.getpass(f"Enter password for {sender_email}: ")
     receiver_email = input("Enter receiver's email id: ")
     receiver = receiver_email.split('@')[0]
     receiver = "".join([i for i in receiver if not i.isdigit()])
-    sender = 'AJ'
-    return sender_email, sender, receiver_email, receiver
+    sender = sender_email.split('@')[0]
+    return sender_email, sender_pwd, sender, receiver_email, receiver
 
 
-def send_mail(message, message_type, sender_email, sender, receiver_email, receiver):
+def send_mail(message, message_type, sender_email, sender, receiver_email, receiver, sender_pwd):
 
     message = f"""From: {sender} <{sender_email}>
 To: {receiver} <{receiver_email}>
@@ -43,7 +43,7 @@ Subject: {message_type}
 """
     s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     message = message.encode("utf8")
-    s.login("rj322198@gmail.com", "9557659432.Rj")
+    s.login(sender_email, sender_pwd)
     s.sendmail(sender_email, receiver_email, message)
     s.quit()
 
@@ -90,20 +90,19 @@ def get_weather(browser):
 
 def get_headlines(browser):
 
-    headlines = " Here are the Current News Headlines for you\n"
+    headlines = "Here are the Current News Headlines for you\n"
     print("Headlines\n")
     news = browser.find_element_by_xpath(
         "/html/body/c-wiz/div/div[2]/div[2]/div/main/c-wiz/div[1]/div[1]/div[1]/h2/span/a").click()
-    # print(news)
     time.sleep(5)
     for i in range(10):
         head = browser.find_element_by_xpath(f"/html/body/c-wiz[2]/div/div[2]/c-wiz/div/div[2]/div[2]/span[1]/div/div/main/c-wiz/div/div/main/div[1]/div[{i+1}]/div/article/h3/a")
-        if head.text[-3:-1] == '..':
+        if '...' in head.text:
             head = browser.find_element_by_xpath(f"/html/body/c-wiz[2]/div/div[2]/c-wiz/div/div[2]/div[2]/span[1]/div/div/main/c-wiz/div/div/main/div[1]/div[{i+1}]/div/article/a").get_attribute("href")
             browser.execute_script("window.open('');")
             browser.switch_to.window(browser.window_handles[1])
             browser.get(head)
-            time.sleep(2)
+            time.sleep(3)
             headline = browser.title
             browser.close()
             browser.switch_to.window(browser.window_handles[0])
